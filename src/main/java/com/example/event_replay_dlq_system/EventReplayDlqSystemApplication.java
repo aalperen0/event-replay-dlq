34a4fe570a4da2718a75248fde8294e4f894d791
Alperen <aalperen.yucel@gmail.com>
2025-10-04 06:23:06 +0300

    start

---
+++ src/main/java/com/example/event_replay_dlq_system/EventReplayDlqSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_replay_dlq_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReplayDlqSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventReplayDlqSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/event_replay_dlq_system/EventReplayDlqSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_replay_dlq_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReplayDlqSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
